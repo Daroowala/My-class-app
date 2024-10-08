f727cd1578e453718e8c0af1fafee9270c38a990
Daroowala <153667031+Daroowala@users.noreply.github.com>
2024-10-08 17:17:40 +0500

    button done

---
+++ src/components/ButtonCom.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ButtonCom = ({label, onClick}) => {
+  return (
+    
+      <button onClick={onClick}>{label}
+    </button>
+  )
+}
+
+export default ButtonCom
