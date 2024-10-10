ef5bc4f13c8b033009ba8774eff9c2944690b08b
Daroowala <153667031+Daroowala@users.noreply.github.com>
2024-10-10 18:22:54 +0500

    trying with tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import ButtonCom from './components/ButtonCom';
 import InputCom from './components/InputCom';
+import Navbar from './Layout/Navbar';
+import './index.css';
 import './App.css'; 
 
  
@@ -20,11 +22,14 @@ function App() {
   return (
     <>
     <div>
+    <h1 className='text-[red] p-10'>Hello Tailwind</h1>
       <InputCom />
       <ButtonCom label="Submit" onClick={SubmitButtonClick} />
       <ButtonCom label="Reset" onClick={ResetButtonClick} />
       <ButtonCom label="Login" onClick={LoginButtonClick} />
+      <Navbar />
     </div>
+    
     </>
   );
 }
